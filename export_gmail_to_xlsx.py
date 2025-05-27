7f269a0f3e6c4d88bddb61cab329724d9c173891
pasatsanduadrian <pasatsandu@gmail.com>
2025-05-27 10:40:42 +0300

    Update export_gmail_to_xlsx.py

--- export_gmail_to_xlsx.py
+++ export_gmail_to_xlsx.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from gmail_utils import get_gmail_service
 
-def fetch_all_inbox_messages(service):
-    # Colectează toate mesajele din Inbox, nu doar primele 500
+def fetch_inbox_messages(service, inbox_max=1000):
+    """
+    Returnează primele `inbox_max` mesaje din inbox, folosind paginare.
+    """
     all_msgs = []
     page_token = None
     while True:
@@ -11,21 +12,31 @@ def fetch_all_inbox_messages(service):
         ).execute()
         messages = response.get('messages', [])
         all_msgs.extend(messages)
+        # Dacă am atins limita cerută de utilizator, ne oprim
+        if inbox_max and len(all_msgs) >= inbox_max:
+            all_msgs = all_msgs[:inbox_max]
+            break
         page_token = response.get('nextPageToken')
         if not page_token:
             break
     return all_msgs
 
-def export_labels_and_inbox_xlsx(service, xlsx_path="gmail_labels_inbox.xlsx"):
-    # Export labels (Sheet: Labels)
+def export_labels_and_inbox_xlsx(service, xlsx_path="gmail_labels_inbox.xlsx", inbox_max=1000):
+    """
+    Exportă etichetele și primele `inbox_max` mailuri din Inbox într-un fișier XLSX cu 2 sheet-uri.
+    """
+    # Export labels
     labels = service.users().labels().list(userId='me').execute().get('labels', [])
     df_labels = pd.DataFrame([{"Label": l["name"], "ID": l["id"]} for l in labels if l.get("type") == "user"])
 
-    # Export inbox (Sheet: Inbox) - toate mesajele
-    messages = fetch_all_inbox_messages(service)
+    # Export inbox (max inbox_max mailuri)
+    messages = fetch_inbox_messages(service, int(inbox_max))
     emails = []
     for m in messages:
-        msg = service.users().messages().get(userId='me', id=m['id'], format='metadata', metadataHeaders=['From','Subject','Date']).execute()
+        msg = service.users().messages().get(
+            userId='me', id=m['id'],
+            format='metadata', metadataHeaders=['From','Subject','Date']
+        ).execute()
         headers = {h['name']: h['value'] for h in msg.get('payload', {}).get('headers', [])}
         emails.append({
             "ID": m['id'],
